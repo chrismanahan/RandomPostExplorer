16f8052a781a329ffc145e661a208742eebc4098
Chris Manahan <chrismanahan.dev@gmail.com>
2016-02-22 13:50:51 -0500

    Copied over sqlite version from pervious tutorial. Use this as your starting point for this tutorial

---
+++ RandomPostExplorerServer/RandomPostExplorerServer.h
@@ -0,0 +1,19 @@
+//
+//  RandomPostExplorerServer.h
+//  RandomPostExplorerServer
+//
+//  Created by Chris M on 2/22/16.
+//  Copyright © 2016 Christopher Manahan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for RandomPostExplorerServer.
+FOUNDATION_EXPORT double RandomPostExplorerServerVersionNumber;
+
+//! Project version string for RandomPostExplorerServer.
+FOUNDATION_EXPORT const unsigned char RandomPostExplorerServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RandomPostExplorerServer/PublicHeader.h>
+
+
